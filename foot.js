e9598f060eea3bd8a73f726e3492ebcd2888c2e7
JunZee002 <81413037+JunZee002@users.noreply.github.com>
2024-08-14 21:34:54 +0600

    Create foot.js

---
+++ foot.js
@@ -0,0 +1,10 @@
+// footer.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = `
+        <footer>
+            <lol>
+        </footer>
+    `;
+    document.body.insertAdjacentHTML('beforeend', footer);
+});
