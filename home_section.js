d84d47c46feb2bbc2b96234741fdbf2c429b3c7d
JunZee002 <81413037+JunZee002@users.noreply.github.com>
2024-08-15 15:56:54 +0600

    Update home_section.js

--- home_section.js
+++ home_section.js
@@ -1,55 +1,126 @@
-function loadHomeSection() {
-    const homeSectionHTML = `
-        <section class="home" id="home">
-            <div class="swiper home-slider">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="box" style="background: url(https://cdn.comedy.co.uk/images/channels/tnt_sports_1.jpg) no-repeat; cursor: pointer;" onclick="location.href='Sports/TNTSports1.html';"></div>
-                        <div class="content" style="text-align: left;">
-                            <h3>TNT Sports 1</h3>
-                            <a href="Sports/TNTSports1.html" class="btn">Watch</a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="box second" style="background: url('img/home-img-3.jpeg') no-repeat;">
-                            <div class="content">
-                                <h3>DEATH NOTE</h3>
-                                <p>When a Japanese high schooler comes into possession of a mystical<br>
-                                notebook, he finds he has the power to kill anybody whose name he<br>
-                                enters in it.</p>
-                                <a href="#" class="btn">Download</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="box" style="background: url('img/home-img-4.jpeg') no-repeat;">
-                            <div class="content">
-                                <h3>One-Punch Man</h3>
-                                <p>The most powerful superhero in the world can kill anyone with one<br>
-                                blow. But nothing can challenge him, so he struggles with ennui and<br>
-                                depression</p>
-                                <a href="#" class="btn">Download</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="box" style="background: url('img/home-img-2.jpeg') no-repeat;">
-                            <div class="content">
-                                <h3>Naruto</h3>
-                                <p>Guided by the spirit demon within him, orphaned Naruto learns<br>
-                                to harness his powers as a ninja in this anime adventure series.</p>
-                                <a href="#" class="btn">Download</a>
-                            </div>
-                        </div>
-                    </div>
+<section class="anime" id="anime">
+    <h1 class="heading">Popular Channels</h1>
+    <div class="swiper anime-slider">
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(https://cdn.comedy.co.uk/images/channels/tnt_sports_1.jpg) no-repeat; cursor: pointer;" onclick="location.href='Sports/TNTSports1.html';"></div>
+                <div class="content" style="text-align: left;">
+                    <h3>TNT Sports 1</h3>
+                    <a href="Sports/TNTSports1.html" class="btn">Watch</a>
                 </div>
             </div>
-        </section>
-    `;
 
-    // Append the generated HTML to the body or a specific element
-    document.getElementById('home-section-container').innerHTML = homeSectionHTML;
-}
+            <div class="swiper-slide">
+                <div class="box" style="background: url(https://w7.pngwing.com/pngs/214/781/png-transparent-star-sports-sony-ten-star-india-television-channel-others-miscellaneous-television-text.png) no-repeat; cursor: pointer;" onclick="location.href='channels/Sports/StarSports1HD.html';"></div>
+                <div class="content">
+                    <h3>Star Sports 1 HD</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="channels/Sports/StarSports1HD.html" class="btn">Watch</a>
+                </div>
+            </div>
 
-// Call the function to load the home section
-document.addEventListener('DOMContentLoaded', loadHomeSection);
+            <div class="swiper-slide">
+                <div class="box" style="background: url(https://upload.wikimedia.org/wikipedia/tr/6/69/Discovery_HD_logo.PNG) no-repeat; cursor: pointer;" onclick="location.href='Infotainment/DiscoveryHD.html';"></div>
+                <div class="content">
+                    <h3>Discovery HD</h3>
+                  
+                    <a href="Infotainment/DiscoveryHD.html" class="btn">Watch</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-4.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Black Clover</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-5.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Jujutsu Kaisen</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-6.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Haikyu!!</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-7.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Kuroko's Basketball</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-8.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>My Hero Academia</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-9.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Dr. Stone</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-10.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Tokyo Revengers</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-11.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>Sword Art Online</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="#" class="btn">Download</a>
+                </div>
+            </div>
+
+            <div class="swiper-slide">
+                <div class="box" style="background: url(img/anime-img-12.jpg) no-repeat;"></div>
+                <div class="content">
+                    <h3>KENGAN ASHURA</h3>
+                    <p>Guided by the spirit demon within him, orphaned Naruto learns
+                    to harness his powers as a ninja in this anime adventure series.</p>
+                    <a href="download.html" class="btn">Download</a>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</section>
