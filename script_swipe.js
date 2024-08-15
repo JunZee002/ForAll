6f561bd77d24dbe8185c681daa55a5dcc3a1e4c6
JunZee002 <kcj.dark.phoenix.002@gmail.com>
2024-08-16 02:29:53 +0600

    home

---
+++ script_swipe.js
@@ -0,0 +1,152 @@
+const swiperhome = new Swiper('#swiperhome', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiperhome .swiper-button-next',
+        prevEl: '#swiperhome .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiperhome .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 1, // Number of slides visible at once
+    spaceBetween: 30, // Space between slides
+});
+
+const swiper1 = new Swiper('#swiper1', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper1 .swiper-button-next',
+        prevEl: '#swiper1 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper1 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper2 = new Swiper('#swiper2', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper2 .swiper-button-next',
+        prevEl: '#swiper2 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper2 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper3 = new Swiper('#swiper3', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper3 .swiper-button-next',
+        prevEl: '#swiper3 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper3 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper4 = new Swiper('#swiper4', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper4 .swiper-button-next',
+        prevEl: '#swiper4 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper4 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper5 = new Swiper('#swiper5', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper5 .swiper-button-next',
+        prevEl: '#swiper5 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper5 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper6 = new Swiper('#swiper6', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper6 .swiper-button-next',
+        prevEl: '#swiper6 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper6 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper7 = new Swiper('#swiper7', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper7 .swiper-button-next',
+        prevEl: '#swiper7 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper7 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
+
+const swiper8 = new Swiper('#swiper8', {
+    loop: true, // Enables looping of slides
+    autoplay: {
+        delay: 2000, // Delay in ms between slides
+    },
+    navigation: {
+        nextEl: '#swiper8 .swiper-button-next',
+        prevEl: '#swiper8 .swiper-button-prev',
+    },
+    pagination: {
+        el: '#swiper8 .swiper-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6, // Number of slides visible at once
+    spaceBetween: 10, // Space between slides
+});
